95d9840dc70862b55c0c788bdc0c8fa5b728fa52
dmonastyrskiy <38734071+dmonastyrskiy@users.noreply.github.com>
2018-04-25 22:53:28 +0300

    Css, and other stuff

--- index.php
+++ index.php
@@ -19,12 +19,14 @@ else {
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
 	<title>Система тестирования</title>
 	<link rel="stylesheet" href="css/style.css">
+	
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
-body {margin:0;}
+body {margin:0;background-color:#58728c;}
 </style>
 </head>
 <body>
@@ -48,12 +50,68 @@ body {margin:0;}
   <a href="#Главная" name="main">Главная</a>
   <a href="#Тесты" name="tests">Тесты</a>
   <a style="float:right" href="" onclick="" id="logoutBtn">Выход</a>
-
 </div>
 
-<div class="main">
-	<p>Вы вошли в систему</p>
+
+<!--Обертка  -->
+<div class="wrapper">
+	
 </div>
+
+
+<!--Контент  -->
+
+	<!--Ряд   -->
+	<div class="row">
+	
+		<!--Панель пользователя   -->
+		<div class="side">
+		    <h2 id="username">Даниил Монастырский</h2>
+				<div class="fakeimg">
+					<img src="images/noavatar.png">
+					</img>
+				</div>
+			<p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+			<h3>More Text</h3>
+			<p>Lorem ipsum dolor sit ame.</p>
+			<div class="fakeimg" style="height:60px;">Тесты</div><br>
+			<div class="fakeimg" style="height:60px;">История</div><br>
+			<div class="fakeimg" style="height:60px;">Профиль</div>
+		</div>
+		<div class="line">
+		</div>
+		<!--Тесты   -->
+		<div class="main">
+		     <h2>TITLE HEADING</h2>
+			<h5>Title description, Dec 7, 2017</h5>
+			<div class="fakeimg" style="height:200px;">Image</div>
+			<p>Some text..</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<br>
+			<h2>TITLE HEADING</h2>
+			<h5>Title description, Sep 2, 2017</h5>
+			<div class="fakeimg" style="height:200px;">Image</div>
+			<p>Some text..</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+		</div>
+	</div>
+		
+</div>
+
+
+<!--Футер  -->
+<div class="footer">
+  <p>Footer</p>
+</div>
+
 
 <script src="jquery/jquery.js"></script>
 <script src="scripts/scripts.js"></script>
